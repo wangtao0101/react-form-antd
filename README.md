823b45652d5848ce47617e00e13a55e41e6bdbd3
wangtao0101 <yuecjn@gmail.com>
2017-08-29 12:00:11 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-form-antd
+
+An easy form component based on ant design.
---
+++ stories/index.js
@@ -0,0 +1,5 @@
+// import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+storiesOf('test', module).add('common', () => null);
